b6395aca81fa7e7d1b30f40aeae226b5acc153ef
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-07-25 15:37:23 -0400

    update naming

--- frontend/src/components/base/Dropdown.tsx
+++ frontend/src/components/base/Dropdown.tsx
@@ -17,7 +17,7 @@ const Dropdown = ({ onSelectStatus, listItem }) => {
         <Menu as="div" className="relative inline-block text-left">
             <div>
                 <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    {selectedStatus || listItem.status}
+                    {selectedStatus || 'update'}
                     <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
                 </MenuButton>
             </div>
